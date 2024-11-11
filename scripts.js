ef45f7cc4d9444cc01d19b89c82e1ca2b9e0f7a0
carla magalhaes <carla.magalhaes404@gmai.com>
2024-11-10 21:42:55 -0300

    Criando a função para adicionar uma nova despesa

--- scripts.js
+++ scripts.js
@@ -41,4 +41,15 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
+
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+    try{
+        throw new Error('Erro de teste')
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas")
+        console.log(error)
+    }
 }
